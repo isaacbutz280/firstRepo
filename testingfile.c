b14cf3e4757fa7b0bbc22efadf8044f31786f9f5
Isaac Butz <isaacbutz280@gmail.com>
2021-03-15 13:01:10 -0400

    I am doing my first commit!

---
+++ testingfile.c
@@ -0,0 +1,12 @@
+/* Very fun */
+
+#include <stdio.h>
+
+main() {
+  int i = 5
+  printf("This is very fun. Lets figure out how to make a cleaning script\n");
+  while (i > 0) {
+    printf("%d", i);
+    i++;
+  }
+}
